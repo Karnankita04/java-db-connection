c5a2caba112b615356c4b387822fbdfb1568ab3f
karnankita04 <ankitakarn04@gmail.com>
2025-06-08 13:22:35 +0530

    fetching all the data from db

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,7 @@ public class Main {
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String user = "postgres";
         String password = "Ankitakarn";
-        String sql = "SELECT * FROM Student\n" +
-                "WHERE id = 2;";
+        String sql = "SELECT * FROM Student";
 
         Connection con = DriverManager.getConnection(url,user,password);
         System.out.println("Connection Established");
@@ -27,13 +26,19 @@ public class Main {
         System.out.println("Statement is created");
 
         ResultSet rs = st.executeQuery(sql); // to fetch data (executeQuery)
-        System.out.println(rs.next());
+
+//        rs.next();
+//        String name = rs.getString("name");
+//        System.out.println("Name of a student is : " + name);
+
+        while(rs.next()){
+            System.out.print(rs.getInt("id") + " - ");
+            System.out.print(rs.getString("name") + " - ");
+            System.out.println(rs.getInt("age"));
+
+        }
 
         con.close();
         System.out.println("Connection closed");
-
-
-
-
     }
 }
